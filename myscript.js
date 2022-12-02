0a64ead45a340fcba2896d1183fe426701c920f8
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2022-12-02 16:04:04 +0100

    getting DOM element

--- myscript.js
+++ myscript.js
@@ -11,3 +11,7 @@ Infine scrivi sulla pagina nomecognomecolorepreferito21
 // .3 Preparo variabile cognome
 // .4 Preparo variabile colore preferito
 // .6 immetto nel DOM la password generata
+
+// .1 Prendo l'elemento dal DOM
+const epassword = document.getElementById("password");
+console.log(epassword);
