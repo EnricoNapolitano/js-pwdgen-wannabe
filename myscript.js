f34b7994bced509d5039f335622bb214ab5e9fdc
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2022-12-02 15:57:17 +0100

    firsrt analysis

--- myscript.js
+++ myscript.js
@@ -1,1 +1,13 @@
-console.log("js test");
+/* Traccia:
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito21
+*/
+
+//? ANALISI
+// .1 Prendo l'elemento dal DOM
+// .2 Preparo variabile nome utente
+// .3 Preparo variabile cognome
+// .4 Preparo variabile colore preferito
+// .6 immetto nel DOM la password generata
