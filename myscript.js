e7eb45e9f0469012b35973e9705682b095433661
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2022-12-02 16:20:31 +0100

    created const fixedNumber

--- myscript.js
+++ myscript.js
@@ -10,7 +10,9 @@ Infine scrivi sulla pagina nomecognomecolorepreferito21
 // .2 Preparo variabile nome utente
 // .3 Preparo variabile cognome
 // .4 Preparo variabile colore preferito
-// .6 immetto nel DOM la password generata
+// .5 Creo variabile constante con valore di tipo stringa "21"
+// .6 faccio generare la password
+// .7 immetto nel DOM la password generata
 
 // .1 Prendo l'elemento dal DOM
 const password = document.getElementById("password");
@@ -27,3 +29,7 @@ const lastName = prompt("Qual è il tuo cognome?");
 // .4 Preparo variabile colore preferito
 const favColor = prompt("Scrivi il colore che più ti piace :)");
 // // console.log(favColor);
+
+// .5 Creo variabile constante con valore di tipo stringa "21"
+const fixedNumber = "21";
+// // console.log(fixedNumber);
